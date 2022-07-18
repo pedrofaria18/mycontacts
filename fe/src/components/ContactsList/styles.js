f8395a236f8b4d9046e85f8c444d8e46506f4e87
pedro <pedrof.andrade16@gmail.com>
2022-07-18 19:26:26 -0300

    Criando a listagem de contatos

--- fe/src/components/ContactsList/styles.js
+++ fe/src/components/ContactsList/styles.js
@@ -11,7 +11,7 @@ export const Header = styled.header`
 
   strong {
     font-size: 24px;
-    color: #222;
+    color: ${({ theme }) => theme.colors.gray[900]};
   }
 
   a {
@@ -34,6 +34,92 @@ export const Header = styled.header`
     &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #FFF;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background: transparent;
+
+      border: none;
+
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-right: 8px;
+
+        font-weight: bold;
+
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #FFF;
+
+  box-shadow: 0px 4px 10px rgba(0,0,0,0.0.4);
+
+  padding: 16px;
+
+  border-radius: 4px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+
+        color: ${({ theme }) => theme.colors.primary.main};
+
+        font-weight: bold;
+
+        text-transform: uppercase;
+
+        padding: 4px;
+
+        border-radius: 4px;
+
+        margin-left: 8px;
+      }
+    }
+
+    span {
+      display: block;
+
+      color: ${({ theme }) => theme.colors.gray[200]};
+
+      font-size: 14px;
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
     }
   }
 `;
