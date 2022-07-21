aa060943ed24095eb26872eb281c337a5a401d68
pedro <pedrof.andrade16@gmail.com>
2022-07-21 17:32:20 -0300

    Criando a página Home e refatorando o Header

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,3 +1,76 @@
+import arrow from '../../assets/images/arrow.svg';
+import edit from '../../assets/images/edit.svg';
+import trash from '../../assets/images/trash.svg';
+
+import {
+  Card, Container, InputSearchContainer, Header, ListContainer,
+} from './styles';
+
 export default function Home() {
-  return 'Homepage';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Ícone de flexa" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Pedro Faria</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>pedro@gmail.com</span>
+            <span>(31) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Ícone de edição" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Ícone de exclusão" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Pedro Faria</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>pedro@gmail.com</span>
+            <span>(31) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Ícone de edição" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Ícone de exclusão" />
+            </button>
+          </div>
+        </Card>
+
+      </ListContainer>
+    </Container>
+  );
 }
