e247287ab42500399cecb3cb4f09512e21bbed0c
pedro <pedrof.andrade16@gmail.com>
2022-07-21 21:45:58 -0300

    Status de danger no Modal

--- fe/src/components/Modal/index.js
+++ fe/src/components/Modal/index.js
@@ -1,20 +1,30 @@
+import PropTypes from 'prop-types';
+
 import Button from '../Button';
 
 import { Overlay, Container, Footer } from './styles';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Título do modal</h1>
 
         <p>Corpo do modal</p>
 
         <Footer>
           <button type="button" className="cancel-button">Cancelar</button>
-          <Button type="button">Deletar</Button>
+          <Button type="button" danger={danger}>Deletar</Button>
         </Footer>
       </Container>
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -11,7 +11,7 @@ import {
 export default function Home() {
   return (
     <Container>
-      <Modal />
+      <Modal danger />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
